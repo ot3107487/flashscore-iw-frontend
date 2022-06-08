c46d92bb9f85809a0cebb6dc05038a6d2c5a06b7
Kocsis Saul <ksaulfelix@yahoo.com>
2022-06-08 18:49:55 +0300

    event align bug solved

--- src/app/modules/match/components/match-view/match-view.component.ts
+++ src/app/modules/match/components/match-view/match-view.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
 import { MatchService } from '../../services/match.service';
 
 @Component({
